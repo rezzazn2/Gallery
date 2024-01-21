f3e3a3a7149d44fed20359d18d016537d390ac23
rezzazn2 <136776620+rezzazn2@users.noreply.github.com>
2024-01-21 20:09:23 +0700

    hapus tambah
    hapus adminLTE, tambah gallery

---
+++ public/gallery/script.js
@@ -0,0 +1,44 @@
+// navbar
+const nav = document.querySelector(".header")
+
+function changeHeightNav() {
+    if (window.scrollY > 1) {
+        nav.classList.add('lebih')
+    } else {
+        nav.classList.remove('lebih')
+    }
+}
+
+window.addEventListener("scroll", changeHeightNav);
+
+// dropdown
+
+const dropdown = document.querySelectorAll('.dropdown');
+
+dropdown.forEach(dropdown =>{
+    const select = dropdown.querySelector('.select');
+    const caret = dropdown.querySelector('.caret');
+    const menu = dropdown.querySelector('.menu');
+    const options = dropdown.querySelectorAll('.menu li');
+    const selected = dropdown.querySelector('.selected');
+
+    select.addEventListener('click', () =>{
+        select.classList.toggle('select-clicked')
+        caret.classList.toggle('caret-rotate')
+        menu.classList.toggle('menu-open')
+    })
+
+    options.forEach(option => {
+        option.addEventListener('click', () =>{
+            selected.innerText = option.innerText
+            select.classList.remove('select-clicked')
+            caret.classList.remove('caret-rotate')
+            menu.classList.remove('menu-open')
+
+            options.forEach(option => {
+                option.classList.remove('active')
+            })
+            option.classList.add('active')
+        })
+    })
+})
---
+++ resources/views/gallery/template/footer.blade.php
@@ -0,0 +1,4 @@
+</body>
+<script src="gallery/script.js"></script>
+<script src="https://kit.fontawesome.com/18c9c6e968.js" crossorigin="anonymous"></script>
+</html>
